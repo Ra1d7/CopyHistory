71d61611232a007c4756fa7401ca9e1be63bec53
Raid7 <thmrgb8@gmail.com>
2022-11-18 01:30:28 +0300

    Add files via upload

---
+++ CopyHistory/CopyHistory/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CopyHistory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 225);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 325);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(91, 36);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Clear";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(252, 325);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(91, 36);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Shortcuts";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(373, 384);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(373, 384);
+            this.MinimumSize = new System.Drawing.Size(373, 384);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Text = "Copy History";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ CopyHistory/CopyHistory/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyHistory
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Dictionary<string,DateTime> dateofitem = new Dictionary<string,DateTime>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string cur_clip = Clipboard.GetText().ToString();
+            if (listBox1.Items.Contains(cur_clip))
+            {
+                //
+            }
+            else
+            {
+                if (cur_clip.Length != 0)
+                {
+                    listBox1.Items.Add(cur_clip);
+                    try
+                    {
+                        dateofitem.Add(cur_clip, DateTime.Now);
+                    }
+                    catch
+                    {
+                        //
+                    }
+                    
+                }
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(listBox1.SelectedItem.ToString());
+                MessageBox.Show("Copied!");
+            }
+            catch
+            {
+                MessageBox.Show("Could Not Perform hotkey, make sure an item is selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                switch (e.KeyChar.ToString().ToLower())
+                {
+                    case "h":
+                        ShowHelp();
+                        break;
+                    case "f":
+                        MessageBox.Show(listBox1.SelectedItem.ToString());
+                        break;
+                    case "d":
+                        listBox1.Items.Remove(listBox1.SelectedItem);
+                        break;
+                    case "t":
+                        MessageBox.Show(dateofitem[listBox1.SelectedItem.ToString()].ToString());
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            ShowHelp();   
+        }
+
+        private void ShowHelp()
+        {
+            MessageBox.Show("- Pressing F will show the full text of the selected item\n- Pressing D will delete the selected item\n- Pressing H will show this menu\n- Pressing T will show the time you copied the item\n- Double clicking an item will copy it", "Useful Shortcuts:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowHelp();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+        }
+    }
+}
